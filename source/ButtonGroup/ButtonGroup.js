a8c28df6aca1331b5608f89f5dbfb14979a3e470
Adam Royle <adam@bigfish.tv>
2018-04-12 21:11:44 +1000

    Add ButtonGroup test and define functionality

--- source/ButtonGroup/ButtonGroup.js
+++ source/ButtonGroup/ButtonGroup.js
@@ -5,11 +5,11 @@ const ButtonGroup = ({ className, block, size, pill, square, color, border, chil
   <div {...props} className={classnames('button-group', className, { 'button-group-block': block })}>
     {React.Children.toArray(children).map(child =>
       React.cloneElement(child, {
-        size: size || child.props.size,
-        pill: pill || child.props.pill,
-        square: square || child.props.square,
-        color: color || child.props.color,
-        border: border || child.props.border,
+        size: child.props.size === undefined ? size : child.props.size,
+        pill: child.props.pill === undefined ? pill : child.props.pill,
+        square: child.props.square === undefined ? square : child.props.square,
+        color: child.props.color === undefined ? color : child.props.color,
+        border: child.props.border === undefined ? border : child.props.border,
       })
     )}
   </div>
---
+++ source/ButtonGroup/ButtonGroup.test.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { renderToStaticMarkup } from 'react-dom/server'
+
+import ButtonGroup from './ButtonGroup'
+import Button from '../Button'
+
+describe('ButtonGroup', () => {
+  it('renders', () => {
+    const result = renderToStaticMarkup(<ButtonGroup />)
+    const expected = '<div class="button-group"></div>'
+    expect(result).toEqual(expected)
+  })
+
+  it('renders block prop', () => {
+    const result = renderToStaticMarkup(<ButtonGroup block />)
+    const expected = '<div class="button-group button-group-block"></div>'
+    expect(result).toEqual(expected)
+  })
+
+  it('renders className prop', () => {
+    const result = renderToStaticMarkup(<ButtonGroup className="custom" />)
+    const expected = '<div class="button-group custom"></div>'
+    expect(result).toEqual(expected)
+  })
+
+  it('renders children', () => {
+    const result = renderToStaticMarkup(
+      <ButtonGroup>
+        <Button />
+      </ButtonGroup>
+    )
+    const expected = '<div class="button-group"><button type="button"></button></div>'
+    expect(result).toEqual(expected)
+  })
+
+  it('handles false children', () => {
+    const result = renderToStaticMarkup(<ButtonGroup>{false}</ButtonGroup>)
+    const expected = '<div class="button-group"></div>'
+    expect(result).toEqual(expected)
+  })
+
+  it('handles false children with non-false children', () => {
+    const result = renderToStaticMarkup(
+      <ButtonGroup>
+        <Button />
+        {false}
+      </ButtonGroup>
+    )
+    const expected = '<div class="button-group"><button type="button"></button></div>'
+    expect(result).toEqual(expected)
+  })
+
+  it('spreads unknown props to the div', () => {
+    const result = renderToStaticMarkup(
+      <ButtonGroup data-custom-attr="Yeh">
+        <Button />
+      </ButtonGroup>
+    )
+    const expected = '<div data-custom-attr="Yeh" class="button-group"><button type="button"></button></div>'
+    expect(result).toEqual(expected)
+  })
+
+  it('assigns button props to the children', () => {
+    const tree = renderer
+      .create(
+        <ButtonGroup color="primary" size="s" pill square border>
+          <Button />
+          <Button />
+          <Button />
+        </ButtonGroup>
+      )
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('child props take priority over parent props', () => {
+    const tree = renderer
+      .create(
+        <ButtonGroup size="s" color="primary">
+          <Button />
+          <Button size="l" color="secondary" />
+          <Button />
+        </ButtonGroup>
+      )
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
