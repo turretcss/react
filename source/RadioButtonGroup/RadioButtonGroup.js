7f71418b52b98de0e1ad63f7b88b7efcf5960c70
Adam Royle <adam@bigfish.tv>
2018-04-15 20:35:52 +1000

    Add tests for RadioButtonGroup

--- source/RadioButtonGroup/RadioButtonGroup.js
+++ source/RadioButtonGroup/RadioButtonGroup.js
@@ -3,13 +3,12 @@ import React from 'react'
 import Button from '../Button'
 import ButtonGroup from '../ButtonGroup'
 
-const RadioButtonGroup = ({ options = [], color, activeColor = 'primary', disabled, value, onChange, ...props }) => (
+const RadioButtonGroup = ({ options = [], color, activeColor = 'primary', value, onChange, ...props }) => (
   <ButtonGroup {...props}>
     {options.map(option => (
       <Button
         key={option.value}
         text={option.text}
-        disabled={disabled}
         onClick={() => onChange(option.value)}
         color={value == option.value ? activeColor : color}
       />
---
+++ source/RadioButtonGroup/RadioButtonGroup.test.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { renderToStaticMarkup } from 'react-dom/server'
+
+import RadioButtonGroup from './RadioButtonGroup'
+
+const options = [
+  {
+    value: null,
+    text: 'Auto',
+  },
+  {
+    value: 's',
+    text: 'Small',
+  },
+  {
+    value: 'l',
+    text: 'Large',
+  },
+]
+
+describe('RadioButtonGroup', () => {
+  it('renders', () => {
+    const result = renderToStaticMarkup(<RadioButtonGroup />)
+    const expected = '<div class="button-group"></div>'
+    expect(result).toEqual(expected)
+  })
+
+  it('renders block prop', () => {
+    const result = renderToStaticMarkup(<RadioButtonGroup block />)
+    const expected = '<div class="button-group button-group-block"></div>'
+    expect(result).toEqual(expected)
+  })
+
+  it('renders className prop', () => {
+    const result = renderToStaticMarkup(<RadioButtonGroup className="custom" />)
+    const expected = '<div class="button-group custom"></div>'
+    expect(result).toEqual(expected)
+  })
+
+  it('renders options as buttons with "Auto" option selected (button-primary)', () => {
+    const tree = renderer.create(<RadioButtonGroup options={options} />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders options as buttons with "Small" option selected (button-primary)', () => {
+    const tree = renderer.create(<RadioButtonGroup value="s" options={options} />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('passed button props down to children buttons', () => {
+    const tree = renderer.create(<RadioButtonGroup options={options} size="xs" disabled pill square border />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders color and activeColor props', () => {
+    const tree = renderer
+      .create(<RadioButtonGroup options={options} color="primary" activeColor="secondary" />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('calls onChange when input clicked', () => {
+    let newValue = null
+    const testInstance = renderer.create(<RadioButtonGroup onChange={value => (newValue = value)} options={options} />)
+      .root
+    // click button with value "s"
+    testInstance.findAllByType('button')[1].props.onClick()
+    expect(newValue).toEqual('s')
+  })
+})
